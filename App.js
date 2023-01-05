2edbf870e2e22fcff91b909eda1ab0082f542142
Yvo Lourenzo de Castro <47569750+yvolourenzo@users.noreply.github.com>
2023-01-05 14:01:14 -0300

    Versão com Opacidade

--- App.js
+++ App.js
@@ -14,26 +14,70 @@ constructor(props){
     // passa o valor inicial
     LarAnimada: new Animated.Value(150),
     AltAnimada: new Animated.Value(50),
-
+    // começa com ele visivel
+    OpacidadeAnimada:  new Animated.Value(0)
   };
 
+Animated.sequence([
+   
   Animated.timing(
-    // diz a state que vai ser animada
-    this.state.LarAnimada,
-  { 
-    // O valor que ela vai ser
-    toValue: 350,
-    // Duração da animação
-    duration: 500
-  }
-  ).start();
+      this.state.OpacidadeAnimada,
+    {
+      toValue: 1,
+      duration: 1500
+    }
+    ),
+
+    Animated.parallel([
+      
+      Animated.timing(
+        this.state.LarAnimada,{
+          toValue: 300,
+          duration: 2000
+        }
+      ),
+
+      Animated.timing(
+        this.state.AltAnimada,{
+          toValue: 200,
+          duration: 2000
+        }
+      )
+
+    ]),
+
+    Animated.timing(
+      this.state.OpacidadeAnimada,{
+        toValue: 0,
+        duration: 1500
+      }
+    )
+
+
+  ]).start();
+
+  // Animated.timing(
+  //   // diz a state que vai ser animada
+  //   this.state.LarAnimada,
+  // { 
+  //   // O valor que ela vai ser
+  //   toValue: 350,
+  //   // Duração da animação
+  //   duration: 500
+  // }
+  // ).start();
 
 }
   render(){
     return(
         <View style={styles.container}>
             
-            <Animated.View style={{width:  this.state.LarAnimada, height: this.state.AltAnimada, backgroundColor: '#4169E1', justifyContent: 'center'}}>
+            <Animated.View style={{
+               width:  this.state.LarAnimada,
+               height: this.state.AltAnimada,
+               backgroundColor: '#4169E1', 
+               justifyContent: 'center',
+               opacity: this.state.OpacidadeAnimada}}>
                 <Text style={{color: '#FFFFFF', fontSize: 22, textAlign: 'center'}}>Carregando...</Text>
             </Animated.View>
 
